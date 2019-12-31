2dcb0e961d74d3c4d785dc95ad346a25ce65e1b7
Benedikt Vilji Magnússon <benedikt.vilji@gmail.com>
2019-12-31 16:03:45 +0000

    Created raw multiplayer snake game.

--- build/build.js
+++ build/build.js
@@ -1,91 +1,203 @@
-var ColorHelper = (function () {
-    function ColorHelper() {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var w = 25;
+var h = 25;
+var fr = 5;
+var ca = 1;
+var Vec = (function () {
+    function Vec(x, y) {
+        this.x = x;
+        this.y = y;
     }
-    ColorHelper.getColorVector = function (c) {
-        return createVector(red(c), green(c), blue(c));
+    Vec.mod = function (p) {
+        return new Vec((p.x + w) % w, (p.y + h) % h);
     };
-    ColorHelper.rainbowColorBase = function () {
-        return [
-            color('red'),
-            color('orange'),
-            color('yellow'),
-            color('green'),
-            color(38, 58, 150),
-            color('indigo'),
-            color('violet')
-        ];
+    Vec.add = function (p, d) {
+        return new Vec(p.x + d.x, p.y + d.y);
     };
-    ColorHelper.getColorsArray = function (total, baseColorArray) {
-        var _this = this;
-        if (baseColorArray === void 0) { baseColorArray = null; }
-        if (baseColorArray == null) {
-            baseColorArray = ColorHelper.rainbowColorBase();
-        }
-        var rainbowColors = baseColorArray.map(function (x) { return _this.getColorVector(x); });
-        ;
-        var colours = new Array();
-        for (var i = 0; i < total; i++) {
-            var colorPosition = i / total;
-            var scaledColorPosition = colorPosition * (rainbowColors.length - 1);
-            var colorIndex = Math.floor(scaledColorPosition);
-            var colorPercentage = scaledColorPosition - colorIndex;
-            var nameColor = this.getColorByPercentage(rainbowColors[colorIndex], rainbowColors[colorIndex + 1], colorPercentage);
-            colours.push(color(nameColor.x, nameColor.y, nameColor.z));
-        }
-        return colours;
+    Vec.equals = function (p, d) {
+        return p.x == d.x && p.y == d.y;
     };
-    ColorHelper.getColorByPercentage = function (firstColor, secondColor, percentage) {
-        var firstColorCopy = firstColor.copy();
-        var secondColorCopy = secondColor.copy();
-        var deltaColor = secondColorCopy.sub(firstColorCopy);
-        var scaledDeltaColor = deltaColor.mult(percentage);
-        return firstColorCopy.add(scaledDeltaColor);
-    };
-    return ColorHelper;
+    return Vec;
 }());
-var Shapes = (function () {
-    function Shapes() {
+var up = new Vec(0, -1);
+var down = new Vec(0, 1);
+var left = new Vec(-1, 0);
+var right = new Vec(1, 0);
+var oneplayer = 0;
+var gameover = 1;
+var twoplayer = 2;
+var state;
+var snake;
+var candies;
+var Sprite = (function () {
+    function Sprite() {
+        this.body = [];
     }
-    Shapes.star = function (x, y, radius1, radius2, npoints) {
-        var angle = TWO_PI / npoints;
-        var halfAngle = angle / 2.0;
-        var points = new Array();
-        for (var a = 0; a < TWO_PI; a += angle) {
-            var sx = x + cos(a) * radius2;
-            var sy = y + sin(a) * radius2;
-            points.push(createVector(sx, sy));
-            sx = x + cos(a + halfAngle) * radius1;
-            sy = y + sin(a + halfAngle) * radius1;
-            points.push(createVector(sx, sy));
-        }
-        return points;
+    Sprite.prototype.update = function () { };
+    ;
+    Sprite.prototype.draw = function () { };
+    ;
+    Sprite.prototype.contains = function (p) {
+        for (var i = 0; i < this.body.length; i++)
+            if (this.body[i].x == p.x && this.body[i].y == p.y)
+                return true;
+        return false;
     };
-    return Shapes;
+    Sprite.isEmpty = function (sprites, p) {
+        for (var i = 0; i < sprites.length; i++)
+            if (sprites[i].contains(p))
+                return true;
+        return false;
+    };
+    return Sprite;
 }());
-var angle = 0;
-var squares = 10;
-var colors;
+var Snake = (function (_super) {
+    __extends(Snake, _super);
+    function Snake(x, y, d) {
+        var _this = _super.call(this) || this;
+        _this.direction = d;
+        _this.body.push(new Vec(x, y));
+        return _this;
+    }
+    Snake.prototype.draw = function () {
+        push();
+        for (var i = 1; i < this.body.length; i++)
+            drawCell(this.body[i].x, this.body[i].y);
+        fill(200);
+        drawCell(this.body[0].x, this.body[0].y);
+        pop();
+    };
+    ;
+    Snake.prototype.update = function () {
+        var nextHead = Vec.mod(Vec.add(this.body[0], this.direction));
+        this.body.pop();
+        if (this.contains(nextHead))
+            return false;
+        this.body.unshift(nextHead);
+        return true;
+    };
+    Snake.prototype.grow = function () {
+        this.body.push(new Vec(-1, -1));
+    };
+    Object.defineProperty(Snake.prototype, "head", {
+        get: function () {
+            return this.body[0];
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Snake.prototype, "nextHead", {
+        get: function () {
+            return Vec.mod(Vec.add(this.body[0], this.direction));
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Snake;
+}(Sprite));
+var Candies = (function (_super) {
+    __extends(Candies, _super);
+    function Candies() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Candies.prototype.draw = function () {
+        push();
+        fill(255, 0, 0);
+        for (var i = 0; i < this.body.length; i++)
+            drawCell(this.body[i].x, this.body[i].y);
+        pop();
+    };
+    Candies.prototype.add = function (sprites, q) {
+        if (q === void 0) { q = 1; }
+        for (var i = 0; i < q; i++) {
+            var p = void 0;
+            do {
+                p = new Vec(floor(random(0, w)), floor(random(0, h)));
+            } while (Sprite.isEmpty(sprites, p));
+            this.body.push(p);
+        }
+    };
+    Candies.prototype.remove = function (pos) {
+        this.body = this.body.filter(function (p) { return !(Vec.equals(p, pos)); });
+    };
+    return Candies;
+}(Sprite));
+function drawCell(x, y) {
+    rect(width / w * x, height / h * y, width / w, height / h);
+}
+function gameOverScreen() {
+    background(0, 0, 0, 50);
+    textAlign(CENTER);
+    fill(0, 255, 0);
+    text("GAME OVER!", width / 2, height / 2);
+}
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER);
-    colors = ColorHelper.getColorsArray(squares);
+    createCanvas(400, 400);
+    state = oneplayer;
+    switch (state) {
+        case oneplayer:
+            snake = new Snake(floor(w / 2), floor(h / 2), down);
+            candies = new Candies();
+            candies.add([snake], ca);
+            break;
+    }
+    frameRate(fr);
 }
 function draw() {
-    background(51);
-    translate((width / 2), (height / 2));
-    angle = angle + 0.01;
-    rotate(angle);
-    for (var i = 0; i < squares; i++) {
-        strokeWeight(2);
-        stroke(colors[i]);
-        noFill();
-        beginShape();
-        var points = Shapes.star(0, 0, 10 * i, 20 * i, 5);
-        for (var x = 0; x < points.length; x++) {
-            var v = points[x];
-            vertex(v.x, v.y);
-        }
-        endShape(CLOSE);
+    switch (state) {
+        case oneplayer:
+            if (candies.contains(snake.nextHead)) {
+                candies.remove(snake.nextHead);
+                snake.grow();
+                candies.add([snake]);
+            }
+            if (!snake.update()) {
+                gameOverScreen();
+                state = gameover;
+                break;
+            }
+            background(0);
+            candies.draw();
+            fill(200);
+            snake.draw();
+            break;
+        case twoplayer:
+            break;
+        case gameover:
+            gameOverScreen();
+            break;
+    }
+}
+function keyPressed() {
+    switch (keyCode) {
+        case UP_ARROW:
+            if (snake.direction != down)
+                snake.direction = up;
+            break;
+        case DOWN_ARROW:
+            if (snake.direction != up)
+                snake.direction = down;
+            break;
+        case LEFT_ARROW:
+            if (snake.direction != right)
+                snake.direction = left;
+            break;
+        case RIGHT_ARROW:
+            if (snake.direction != left)
+                snake.direction = right;
+            break;
     }
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,33 +1,242 @@
-let angle = 0;
-let squares = 10;
-let colors: p5.Color[];
+//
+// ┏━┳━┳┳━┳┳┳━┳┳┳┓
+// ┃━┫┃┃┃╻┃┗┫━┫┃┃┃
+// ┣━┃┃┃┃╻┃┃┃━╋╋╋┫
+// ┗━┻┻━┻┻┻┻┻━┻┻┻┛
+//
+// By Benedikt Vilji Magnússon
+//
+// TODO:
+// ✔ Handle walls
+// ✔ Sandardize points
+// ✔ Think more about candy generation location
+// * Add UI
+// * implement input queue
+// * Add player concept
+// * Migrate to p5 vectors
+// * unglobalize
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER);
-    colors = ColorHelper.getColorsArray(squares);
-}
 
-function draw() {
+// game settings
+const w = 25;
+const h = 25;
+const fr = 5;
+const ca = 1;
 
-    background(51);
-
-    translate((width / 2), (height / 2));
-    angle = angle + 0.01;
-    rotate(angle);
-
-    for (var i = 0; i < squares; i++) {
-        strokeWeight(2);
-        stroke(colors[i]);
-        noFill();
-        beginShape();
-
-        let points = Shapes.star(0, 0, 10 * i, 20 * i, 5);
-        for (var x = 0; x < points.length; x++) {
-            var v = points[x]
-            vertex(v.x, v.y);
-        }
-        endShape(CLOSE);
+// class for 2d vectors
+// could easilly be replaced by p5's vector
+class Vec {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
     }
 
+    static mod(p: Vec): Vec {
+        return new Vec((p.x + w) % w, (p.y + h) % h);
+    }
+
+    static add(p: Vec, d: Vec): Vec {
+        return new Vec(p.x + d.x, p.y + d.y);
+    }
+
+    static equals(p: Vec, d: Vec): boolean {
+        return p.x == d.x && p.y == d.y;
+    }
+}
+
+// directional constants
+const up: Vec    = new Vec( 0, -1);
+const down: Vec  = new Vec( 0,  1);
+const left: Vec  = new Vec(-1,  0);
+const right: Vec = new Vec( 1,  0);
+
+// state machine
+const oneplayer = 0;
+const gameover = 1;
+const twoplayer = 2;
+let state: number;
+
+// ingame state
+let snake: Snake;
+let candies: Candies;
+
+
+class Sprite {
+    body: Vec[];
+    update(): void {};
+    draw(): void {};
+
+    constructor() {
+        this.body = [];
+    }
+
+    contains(p: Vec): boolean {
+        for (let i = 0; i < this.body.length; i++)
+            if (this.body[i].x == p.x && this.body[i].y == p.y)
+                return true;
+        return false;
+    }
+
+    static isEmpty(sprites: Sprite[], p: Vec) {
+        for (let i = 0; i < sprites.length; i++)
+            if (sprites[i].contains(p))
+                return true;
+        return false;
+    }
+}
+
+class Snake extends Sprite {
+    direction: Vec;
+    constructor(x: number, y: number, d: Vec) {
+        super();
+        this.direction = d;
+        this.body.push(new Vec(x, y));
+    }
+
+    draw(): void {
+        push();
+        for (let i = 1; i < this.body.length; i++)
+            drawCell(this.body[i].x, this.body[i].y);
+        fill(200);
+        drawCell(this.body[0].x, this.body[0].y);
+        pop();
+    };
+
+    update(): boolean {
+        let nextHead = Vec.mod(Vec.add(this.body[0], this.direction));
+        this.body.pop();
+        if (this.contains(nextHead))
+            return false;
+        this.body.unshift(nextHead);
+        return true;
+    }
+
+    grow(): void {
+        this.body.push(new Vec(-1, -1));
+    }
+
+    get head(): Vec {
+        return this.body[0];
+    }
+
+    get nextHead(): Vec {
+        return Vec.mod(Vec.add(this.body[0], this.direction));
+    }
+}
+
+class Candies extends Sprite {
+    draw(): void {
+        push();
+        fill(255, 0, 0);
+        for (let i = 0; i < this.body.length; i++)
+            drawCell(this.body[i].x, this.body[i].y);
+        pop();
+    }
+
+    add(sprites: Sprite[], q: number = 1): void {
+        for (let i = 0; i < q; i++) {
+            let p: Vec;
+            do {
+                p = new Vec(floor(random(0, w)), floor(random(0, h)));
+            } while (Sprite.isEmpty(sprites, p))
+            this.body.push(p);
+        }
+    }
+
+    remove(pos: Vec): void {
+        this.body = this.body.filter(p => !(Vec.equals(p, pos)));
+    }
+}
+
+// draw cell by grid coordinates x and y
+function drawCell(x: number, y: number) {
+    rect(width / w * x, height / h * y, width / w, height / h);
+}
+
+function gameOverScreen() {
+    background(0, 0, 0, 50);
+    textAlign(CENTER);
+    fill(0, 255, 0);
+    text("GAME OVER!", width/2, height/2);
+}
+
+// basic p5 setup
+function setup() { // WARNING: global variables
+    createCanvas(400, 400);
+
+    // state machine setup
+    state = oneplayer;
+
+    switch (state) {
+        case oneplayer:
+            // ingame variable setup
+            snake = new Snake(floor(w / 2), floor(h / 2), down);
+            candies = new Candies();
+            candies.add([snake], ca);
+            break;
+    }
+
+    // slowing down framerate
+    frameRate(fr);
+}
+
+// typical p5 draw loop (game loop)
+function draw() { // WARNING: global variables
+    // check which state the game is in
+    switch (state) {
+        case oneplayer:
+            if (candies.contains(snake.nextHead)) {
+                candies.remove(snake.nextHead);
+                snake.grow();
+                candies.add([snake]);
+            }
+
+            if (!snake.update()) {
+                gameOverScreen();
+                state = gameover;
+                break;
+            }
+
+            // clear screen to black
+            background(0);
+
+            // draw candy
+            candies.draw()
+
+            // draw snake
+            fill(200);
+            snake.draw();
+            break;
+
+        case twoplayer:
+
+            break;
+
+        case gameover:
+            // clear screen to black (with fading affect)
+            gameOverScreen();
+            break;
+    }
+}
+
+// p5 keystroke handeling
+function keyPressed() { // WARNING: global variable
+    // check which key is pressed and change direction accordingly
+    // also checks if you are turning around whitch is ignored
+    switch (keyCode) {
+        case UP_ARROW:
+            if (snake.direction != down) snake.direction = up;
+            break;
+        case DOWN_ARROW:
+            if (snake.direction != up) snake.direction = down;
+            break;
+        case LEFT_ARROW:
+            if (snake.direction != right) snake.direction = left;
+            break;
+        case RIGHT_ARROW:
+            if (snake.direction!= left) snake.direction = right;
+            break;
+    }
 }
